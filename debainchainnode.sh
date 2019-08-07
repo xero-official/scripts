62362465288fabe98e77fc7fe5954c6ea4ace48c
FallenGravity <ethanvb55@gmail.com>
2019-08-07 17:04:05 +0200

    Added 10s Sleep to debain

--- debainchainnode.sh
+++ debainchainnode.sh
@@ -74,6 +74,8 @@ echo '**************************'
 echo 'If you are going to setup your node manually - you can exit the dashboard by using option 5'
 echo '**************************'
 
+sleep 10s
+
 # Activate Dashboard
 ./dashboard
 
